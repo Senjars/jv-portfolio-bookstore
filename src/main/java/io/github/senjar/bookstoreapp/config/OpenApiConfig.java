f03335dcead12e4c2172f87459f410a5cbff29b6
Senjars <jaroslawsendecki1@gmail.com>
2026-05-25 18:19:54 +0200

    feat: add OpenAPI security configuration and optimize docker-compose for deployment

--- src/main/java/io/github/senjar/bookstoreapp/config/OpenApiConfig.java
+++ src/main/java/io/github/senjar/bookstoreapp/config/OpenApiConfig.java
@@ -10,7 +10,7 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class OpenApiConfig {
     @Bean
-    public OpenAPI customOpenAPI() {
+    public OpenAPI customOpenApi() {
         final String securitySchemeName = "bearerAuth";
         return new OpenAPI()
                 .addSecurityItem(new SecurityRequirement()
